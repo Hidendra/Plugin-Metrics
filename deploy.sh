f3619e06c4df5e536216c7f1353205220e737582
Tyler Blair <hidendra@griefcraft.com>
2012-05-19 17:28:16 -0300

    add my deploy script

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# rsync command
+RSYNC="rsync -avzq --progress"
+
+# nginx load balancer
+NGINX_BALANCER="root@lb1.mcstats.org"
+
+cd src
+chmod -R 755 .
+
+# First deploy to the load balancer
+echo "Deploying to nginx load balancer"
+
+# First the main website
+$RSYNC  --exclude 'static/' --exclude 'config.php' ./ $NGINX_BALANCER:/var/www/servers/mcstats.org/
+
+echo -e " \e[0;32m=>\e[00m Main content"
+
+# Static content
+cd static
+$RSYNC ./ $NGINX_BALANCER:/var/www/servers/static.mcstats.org
+cd ..
+
+echo -e " \e[0;32m=>\e[00m Static content"
