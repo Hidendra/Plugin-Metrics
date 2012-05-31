486e4e1dddc3f57b3438e1b6a06c9b1ed381f9a7
Tyler Blair <hidendra@griefcraft.com>
2012-05-31 20:40:26 -0300

    Update deploy script to allow me to more easily deploy to specific realms (live v. test)

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <realm>"
+    exit
+fi
+
+REALM="$1"
+
 # rsync command
 RSYNC="rsync -avzq --progress"
 
 # nginx load balancer
 NGINX_BALANCER="root@mcstats.org"
 
+if [ "$REALM" == "live" ]; then
+    REMOTE_LOCATION="/var/www/servers/mcstats.org/"
+else
+    REMOTE_LOCATION="/var/www/servers/test.mcstats.org/"
+fi
+
 cd src
 chmod -R 755 .
+
+echo -e "Realm: \e[0;32m$REALM\e[00m"
 
 # First deploy to the load balancer
 echo "Deploying to nginx load balancer"
 
 # First the main website
-$RSYNC  --exclude 'static/' --exclude 'config.php' ./ $NGINX_BALANCER:/var/www/servers/mcstats.org/
+$RSYNC  --exclude 'static/' --exclude 'config.php' ./ $NGINX_BALANCER:"$REMOTE_LOCATION"
 
 echo -e " \e[0;32m=>\e[00m Main content"
 
