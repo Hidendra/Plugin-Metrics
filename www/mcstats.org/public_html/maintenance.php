a96a23beb7fc49aeefdad0e735bc154b5cf63143
Tyler Blair <hidendra@griefcraft.com>
2012-08-05 11:22:31 -0300

    minor formatting

--- www/mcstats.org/public_html/maintenance.php
+++ www/mcstats.org/public_html/maintenance.php
@@ -4,48 +4,50 @@
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 
-<head>
-    <meta charset="utf-8" />
-    <title>MCStats :: Maintenance</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description" content="" />
-    <meta name="author" content="Tyler Blair <hidendra@griefcraft.com>" />
+    <head>
+        <meta charset="utf-8" />
+        <title>MCStats :: Maintenance</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="" />
+        <meta name="author" content="Tyler Blair <hidendra@griefcraft.com>" />
 
-    <link href="http://test.static.mcstats.org/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="http://test.static.mcstats.org/css/bootstrap-responsive.min.css" rel="stylesheet" />
-    <link href="http://test.static.mcstats.org/css/ui-lightness/jquery-ui.css" rel="stylesheet" />
+        <link href="http://test.static.mcstats.org/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="http://test.static.mcstats.org/css/bootstrap-responsive.min.css" rel="stylesheet" />
+        <link href="http://test.static.mcstats.org/css/ui-lightness/jquery-ui.css" rel="stylesheet" />
 
-    <script type="text/javascript">
-        // Google analytics
-        var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', 'UA-31036792-1']);
-        _gaq.push(['_setDomainName', 'mcstats.org']);
-        _gaq.push(['_setAllowLinker', true]);
-        _gaq.push(['_trackPageview']);
+        <script type="text/javascript">
+            // Google analytics
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-31036792-1']);
+            _gaq.push(['_setDomainName', 'mcstats.org']);
+            _gaq.push(['_setAllowLinker', true]);
+            _gaq.push(['_trackPageview']);
 
-        (function() {
-            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-        })();
-    </script>
-</head>
-<body>
+            (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+        </script>
+    </head>
 
-<br />
-<div class="container">
-    <div class="hero-unit">
-        <h1 style="margin-bottom:10px; font-size:38px; text-align: center">All new shiny stats.</h1>
-    </div>
+    <body>
 
-    <footer class="row-fluid">
-        <hr />
-        <p> MCStats backend created by Hidendra. Plugins are owned by their respective authors. </p>
-        <p>  <a href="/plugin-list/">plugin list</a> | <a href="/status/">backend status</a> | <a href="/admin/">admin</a> | <a href="http://github.com/Hidendra/mcstats.org">github</a> | irc.esper.net #metrics </p>
-    </footer>
+        <br />
+        <div class="container">
 
-</div>
+            <div class="hero-unit">
+                <h1 style="margin-bottom:10px; font-size:38px; text-align: center">All new shiny stats.</h1>
+            </div>
 
-</body>
+            <footer class="row-fluid">
+                <hr />
+                <p> MCStats backend created by Hidendra. Plugins are owned by their respective authors. </p>
+                <p>  <a href="/plugin-list/">plugin list</a> | <a href="/status/">backend status</a> | <a href="/admin/">admin</a> | <a href="http://github.com/Hidendra/mcstats.org">github</a> | irc.esper.net #metrics </p>
+            </footer>
+
+        </div>
+
+    </body>
 
 </html>
