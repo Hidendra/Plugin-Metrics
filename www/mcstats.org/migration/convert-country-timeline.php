36d41f181a69e7427510feab7f0eb4508ee138b8
Tyler Blair <hidendra@griefcraft.com>
2012-08-09 17:29:47 -0300

    single thread it again

--- www/mcstats.org/migration/convert-country-timeline.php
+++ www/mcstats.org/migration/convert-country-timeline.php
@@ -1,14 +1,10 @@
 <?php
 
 define('ROOT', '../public_html/');
-define('MAX_CHILDREN', 2);
 
 require_once ROOT . 'config.php';
 require_once ROOT . 'includes/database.php';
 require_once ROOT . 'includes/func.php';
-
-// the current number of running forks
-$running_processes = 0;
 
 // number of plugins converted
 $index = 0;
@@ -20,51 +16,29 @@ $total = count($plugins);
 $countries = loadCountries();
 
 // iterate through all of the plugins
+
+$statement = $master_db_handle->prepare('INSERT INTO CustomDataTimeline (Plugin, ColumnID, Sum, Count, Avg, Max, Min, Variance, StdDev, Epoch)
+                                            SELECT Plugin, :ColumnID, Servers, 0, 0, 0, 0, 0, 0, Epoch FROM CountryTimeline where Plugin = :Plugin AND Country = :ShortCode');
 foreach ($plugins as $plugin)
 {
-    // are we at the process limit ?
-    if ($running_processes >= MAX_CHILDREN)
-    {
-        // wait for some children to be allocated
-        pcntl_wait($status);
-        $running_processes --;
+
+    $index++;
+    echo sprintf('[%d%%] Converting %s from CountryTimeline to the unified graphing format ..%s', floor(($index / $total) * 100), $plugin->getName(), PHP_EOL);
+
+    // get or create the graph
+    $serverlocations = $plugin->getOrCreateGraph('Server Locations', false, 1, GraphType::Pie, TRUE);
+
+    $master_db_handle->beginTransaction();
+    foreach ($countries as $shortCode => $countryName) {
+        // get the column id
+        $columnID = $serverlocations->getColumnID($countryName);
+
+        // convert all of it
+        $statement->execute(array(':Plugin' => $plugin->getID(), ':ColumnID' => $columnID, ':ShortCode' => $shortCode));
     }
+    $master_db_handle->commit();
 
-    $running_processes ++;
-    $pid = pcntl_fork();
-    $index ++;
-
-    if ($pid == 0)
-    {
-        $master_db_handle = try_connect_database();
-        echo sprintf('[%d%%] Converting %s from CountryTimeline to the unified graphing format ..%s', floor(($index / $total) * 100), $plugin->getName(), PHP_EOL);
-
-        // get or create the graph
-        $serverlocations = $plugin->getOrCreateGraph('Server Locations', false, 1, GraphType::Pie, TRUE);
-
-        $statement = $master_db_handle->prepare('INSERT INTO CustomDataTimeline (Plugin, ColumnID, Sum, Count, Avg, Max, Min, Variance, StdDev, Epoch)
-                                            SELECT Plugin, :ColumnID, Servers, 0, 0, 0, 0, 0, 0, Epoch FROM CountryTimeline where Plugin = :Plugin AND Country = :ShortCode');
-        $master_db_handle->beginTransaction();
-        foreach ($countries as $shortCode => $countryName)
-        {
-            // get the column id
-            $columnID = $serverlocations->getColumnID($countryName);
-
-            // convert all of it
-            $statement->execute(array(':Plugin' => $plugin->getID(), ':ColumnID' => $columnID, ':ShortCode' => $shortCode));
-        }
-        $master_db_handle->commit();
-
-        echo sprintf('Converted %s%s', $plugin->getName(), PHP_EOL);
-        exit(0);
-    }
-}
-
-// wait for all of the processes to finish
-while ($running_processes > 0)
-{
-    pcntl_wait($status);
-    $running_processes --;
+    echo sprintf('Converted %s%s', $plugin->getName(), PHP_EOL);
 }
 
 echo sprintf('Converted %d plugins%s', count($plugins), PHP_EOL);
--- www/mcstats.org/migration/convert-version-timeline.php
+++ www/mcstats.org/migration/convert-version-timeline.php
@@ -1,14 +1,10 @@
 <?php
 
 define('ROOT', '../public_html/');
-define('MAX_CHILDREN', 2);
 
 require_once ROOT . 'config.php';
 require_once ROOT . 'includes/database.php';
 require_once ROOT . 'includes/func.php';
-
-// the current number of running forks
-$running_processes = 0;
 
 // number of plugins converted
 $index = 0;
@@ -17,52 +13,27 @@ $plugins = loadPlugins(PLUGIN_ORDER_ALPHABETICAL);
 $total = count($plugins);
 
 // iterate through all of the plugins
-foreach ($plugins as $plugin)
-{
-    // are we at the process limit ?
-    if ($running_processes >= MAX_CHILDREN)
-    {
-        // wait for some children to be allocated
-        pcntl_wait($status);
-        $running_processes --;
-    }
-
-    $running_processes ++;
-    $pid = pcntl_fork();
-    $index ++;
-
-    if ($pid == 0)
-    {
-        $master_db_handle = try_connect_database();
-        echo sprintf('[%d%%] Converting %s from VersionTimeline to the unified graphing format ..%s', floor(($index / $total) * 100), $plugin->getName(), PHP_EOL);
-
-        // get or create the graph
-        $versiontrends = $plugin->getOrCreateGraph('Version Trends', false, 1, GraphType::Area, TRUE);
-
-        $statement = $master_db_handle->prepare('INSERT INTO CustomDataTimeline (Plugin, ColumnID, Sum, Count, Avg, Max, Min, Variance, StdDev, Epoch)
+$statement = $master_db_handle->prepare('INSERT INTO CustomDataTimeline (Plugin, ColumnID, Sum, Count, Avg, Max, Min, Variance, StdDev, Epoch)
                                             SELECT Plugin, :ColumnID, Count, 0, 0, 0, 0, 0, 0, Epoch FROM VersionTimeline where Plugin = :Plugin AND Version = :VersionID');
-        $master_db_handle->beginTransaction();
-        foreach ($plugin->getVersions() as $versionID => $versionName)
-        {
-            // get the column id
-            $columnID = $versiontrends->getColumnID($versionName);
+foreach ($plugins as $plugin) {
+    $index++;
+    echo sprintf('[%d%%] Converting %s from VersionTimeline to the unified graphing format ..%s', floor(($index / $total) * 100), $plugin->getName(), PHP_EOL);
 
-            // convert all of it
-            $statement->execute(array(':Plugin' => $plugin->getID(), ':ColumnID' => $columnID, ':VersionID' => $versionID));
-        }
-        $master_db_handle->commit();
+    // get or create the graph
+    $versiontrends = $plugin->getOrCreateGraph('Version Trends', false, 1, GraphType::Area, TRUE);
 
-        echo sprintf('Converted %s%s', $plugin->getName(), PHP_EOL);
-        exit(0);
+    $master_db_handle->beginTransaction();
+    foreach ($plugin->getVersions() as $versionID => $versionName) {
+        // get the column id
+        $columnID = $versiontrends->getColumnID($versionName);
+
+        // convert all of it
+        $statement->execute(array(':Plugin' => $plugin->getID(), ':ColumnID' => $columnID, ':VersionID' => $versionID));
     }
+    $master_db_handle->commit();
 
-}
+    echo sprintf('Converted %s%s', $plugin->getName(), PHP_EOL);
 
-// wait for all of the processes to finish
-while ($running_processes > 0)
-{
-    pcntl_wait($status);
-    $running_processes --;
 }
 
 echo sprintf('Converted %d plugins%s', count($plugins), PHP_EOL);
